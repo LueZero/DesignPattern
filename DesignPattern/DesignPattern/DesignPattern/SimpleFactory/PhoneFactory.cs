161a1a90b45baf912db3553fd39063d7ac49a0de
zero <a0970937127@gmail.com>
2022-02-09 21:44:43 +0800

    create project

---
+++ DesignPattern/DesignPattern/DesignPattern/SimpleFactory/PhoneFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DesignPattern.SimpleFactory
+{
+    public static class PhoneFactory
+    {
+        static PhoneInterface phone;
+
+        public static PhoneInterface GetPhone(string brand)
+        {
+            switch (brand)
+            {
+                case "apple":
+                    phone = new ApplePhone();
+                    break;
+
+                case "samsung":
+                    phone = new Samsung();
+                    break;
+
+                default:
+                    throw new Exception("missing matching brand name");
+            }
+
+            return phone;
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/DesignPattern/SimpleFactory/Samsung.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DesignPattern.SimpleFactory
+{
+    public class Samsung : PhoneInterface
+    {
+        public void Photo()
+        {
+            Console.WriteLine("照相");
+        }
+
+        public void Music()
+        {
+            Console.WriteLine("音樂");
+        }
+    }
+}
