1d4cc8825c6f676c2923bf06b4c4ac4a83a7af1c
zero <a0970937127@gmail.com>
2022-04-05 21:49:44 +0800

    update Flyweight.cs

--- DesignPattern/Flyweight/Test.cs
+++ DesignPattern/Flyweight/Test.cs
@@ -14,13 +14,13 @@ namespace DesignPattern.Flyweight
 
             var phoneFactory = new PhoneFactory();
 
-            var applePhone = phoneFactory.getPhone("Apple");
+            var applePhone = phoneFactory.GetPhone("Apple");
 
             applePhone.Name = "Apple";
             applePhone.System = "IOS";
             applePhone.Reboot();
 
-            var samsungPhon = phoneFactory.getPhone("Apple");
+            var samsungPhon = phoneFactory.GetPhone("Apple");
 
             // 偷偷灌錄三星系統
             samsungPhon.Name = "Samsung";
---
+++ DesignPattern/Proxy/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Proxy
+{
+    class Test
+    {
+        public void Tests()
+        {
+            Console.WriteLine("--模式測試");
+        }
+    }
+}
