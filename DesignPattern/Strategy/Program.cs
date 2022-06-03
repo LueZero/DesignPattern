8fe092d286798bd29dd13fd0fc21ef1441fb277c
zero <a0970937127@gmail.com>
2022-06-03 21:54:19 +0800

    新增策略模式

---
+++ DesignPattern/Strategy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Strategy
+{
+    class Program
+    {
+        private StrategyInterface _strategy;
+
+        public Program()
+        { 
+        }
+
+        public Program(StrategyInterface strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void SetStrategy(StrategyInterface strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Run: Sorting data using the strategy (not sure how it'll do it)");
+
+            var result = this._strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+}
---
+++ DesignPattern/Strategy/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Strategy
+{
+    class Test
+    {
+        public void Tests()
+        {
+            Console.WriteLine("策略模式測試");
+
+            var run = new Program();
+            run.SetStrategy(new ConcreteStrategyA());
+            run.DoSomething();
+
+            run.SetStrategy(new ConcreteStrategyB());
+            run.DoSomething();
+        }
+    }
+}
